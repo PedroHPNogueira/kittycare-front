bc5185f59a86bc9041c310b9862c34c9e37e0526
Pedro Nogueira <pedrophpn2002@hotmail.com>
2025-02-26 02:21:52 -0300

    feat: add prettier and vscode configs

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,8 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
-
+import prettier from 'eslint-config-prettier'
+import eslintPluginPrettier from 'eslint-plugin-prettier'
 export default tseslint.config(
   { ignores: ['dist'] },
   {
@@ -16,6 +17,7 @@ export default tseslint.config(
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      'prettier': eslintPluginPrettier,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
@@ -25,7 +27,8 @@ export default tseslint.config(
       ],
       "@typescript-eslint/no-unused-vars": 'off',
       "@typescript-eslint/no-explicit-any": "off",
-      "react-hooks/exhaustive-deps": "off"
+      "react-hooks/exhaustive-deps": "off",
+      "prettier/prettier": "warn"
     },
   },
 )
